ec9e3e61eb2f538eaefcccc56d911e314832bf0b
Samuel Adekunle <samuelbeebest@gmail.com>
2025-07-20 22:55:58 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Samuel Adekunle on 20/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
